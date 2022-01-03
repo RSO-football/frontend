86dabf9398e4de369ef3aecf284de3acc184e9ed
UniWorksTS <ts4894@student.uni-lj.si>
2022-01-03 18:07:14 +0100

    postavke

--- src/app/postavke/postavke.component.ts
+++ src/app/postavke/postavke.component.ts
@@ -10,6 +10,14 @@ import { FormControl } from '@angular/forms';
 export class PostavkeComponent implements OnInit {
 
     postavke: any[];
+    dolgovi: any[];
+    
+    idControl = new FormControl()
+    postavkaControl = new FormControl()
+    idControl2 = new FormControl()
+    postavkaControl2 = new FormControl()
+    novaControl2 = new FormControl()
+
   constructor(
     private client: HttpClient
   ) { }
@@ -18,6 +26,30 @@ export class PostavkeComponent implements OnInit {
     this.client.get<any>('http://40.76.175.239/postavke/v1/postavke').subscribe(data => {
       console.log(data);
       this.postavke = data;
+    });
+    this.client.get<any>('http://40.76.175.239/postavke/v1/postavke/place').subscribe(data => {
+      console.log(data);
+      this.dolgovi = data;
+    });
+  }
+
+  posodobiPostavko(){
+
+    this.client.put<any>('http://40.76.175.239/postavke/v1/postavke/'+this.postavkaControl2.value,{
+      pay: this.novaControl2.value,
+      uporabnikID: this.idControl2.value
+    }).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ustvariPostavko(){
+
+    this.client.post<any>('http://40.76.175.239/postavke/v1/postavke',{
+      uporabnikID: this.idControl.value,
+      pay: this.postavkaControl.value
+    }).subscribe(data => {
+      console.log(data);
     });
   }
 
